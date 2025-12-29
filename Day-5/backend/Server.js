603edc6e4346949f51ccbac68a172e56f95081e8
AmaraReddy7 <amara@medianv.com>
2025-12-29 23:31:47 +0530

    Day-5 work

---
+++ Day-5/backend/Server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const app = express();
+const port = 3232;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to parker house");
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  // console.log(req.body.email, "email");
+  // console.log("password", req.body.password);
+  res.send("Welcome");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on  http://localhost:${port}`);
+});
---
+++ Day-5/backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const { users } = require("./data");
+const projectRouter = require("./routes/projects");
+const port = 5300;
+
+app.use(express.json());
+
+app.use(setUser);
+app.use("./projects", projectRouter);
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.send("Dashboard Page");
+});
+
+app.get("/Admin", (req, res) => {
+  res.send("Admin Page");
+});
+
+function setUser(req, res, next) {
+  const userId = req.body.userId;
+  if (userId) {
+    req.user = users.find((user) => user.id === user.Id);
+  }
+  next();
+}
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
